0adc7fbab7ed6ab268b2128dc8abfa7d78ca3c4d
RaghadDevoteam <Raghad.aljohani@devoteam.com>
2025-02-16 20:50:50 +0000

    Updated styles and assets

--- app/backend/static/assets/NoPage-C0uV79he.js
+++
@@ -1,2 +0,0 @@
-import{bH as o}from"./vendor-vcBHsUj8.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
-//# sourceMappingURL=NoPage-C0uV79he.js.map
