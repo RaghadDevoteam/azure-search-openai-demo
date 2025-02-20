489519853b78196da3818f80487c73d14e06b43b
RaghadDevoteam <Raghad.aljohani@devoteam.com>
2025-02-20 13:35:32 +0000

    ur esgjsje

--- app/frontend/src/components/Answer/AnswerIcon.tsx
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -1,5 +1,10 @@
 import { Sparkle28Filled } from "@fluentui/react-icons";
 
 export const AnswerIcon = () => {
-    return <Sparkle28Filled primaryFill={"rgba(255, 255, 255, 1)"} aria-hidden="true" aria-label="Answer logo" />;
+    return <Sparkle28Filled 
+    primaryFill={"rgba(255, 255, 255, 1)"} 
+    aria-hidden="true" 
+    aria-label="Answer logo"
+    style={{ stroke: "black", strokeWidth: 2 }} 
+  />;
 };
